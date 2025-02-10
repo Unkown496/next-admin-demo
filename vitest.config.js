958881a84fff44ca45dc41dad0acdfdb87c600b4
Unkown496 <kroloeden@gmail.com>
2025-02-10 23:53:07 +0300

    Правка игнора coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,9 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
-import jsconfigPaths from 'vite-jsconfig-paths';
-import react from '@vitejs/plugin-react';
+import jsconfigPaths from "vite-jsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [jsconfigPaths(), react()],
@@ -12,30 +12,41 @@ export default defineConfig({
     coverage: {
       exclude: [
         // ignore all files bounding of server init
-        '.adminjs',
-        '.next',
-        'server.js',
-        'locales',
-        'public',
-        'prisma',
-        'generate',
+        ".adminjs",
+        ".next",
+        "server.js",
+        "locales",
+        "public",
+        "prisma",
+        "generate",
 
         // ignore root layout
-        'src/app/layout.jsx',
-        'src/app/layout.js',
+        "src/app/layout.jsx",
+        "src/app/layout.js",
 
         // ignore server utils func
-        'utils/**/*.js',
+        "utils/**/*.js",
 
         // configs
-        '**/*.config.js',
-        '**/vitest.*.js',
+        "**/*.config.js",
+        "**/vitest.*.js",
+
+        // mocks files
+        "**/__mocks__/**/*.js",
+        "**/__mocks__/**/*.jsx",
+
+        // small helpers func
+        "**/helpers/**/*.js",
+        "utils/**/*.js",
+
+        // seeds
+        "**/seeds/**/*.js",
       ],
     },
   },
 
   define: {
-    'import.meta.env': {
+    "import.meta.env": {
       ...process.env,
     },
   },
